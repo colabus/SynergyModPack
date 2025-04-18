1b0e8eca3a222b2912484417e6e10a6c42b2c0fc
colabus <colabus@gmail.com>
2025-04-18 10:05:06 +1000

    Syn_EternalFireplace
    Added Syn_EternalFireplace

---
+++ config.cpp
@@ -0,0 +1,34 @@
+﻿class CfgMods
+{
+	class SynergyModPack
+	{
+		dir = "SynergyModPack";
+		name = "";
+		author = "colabus";
+		credits = "";
+		version = "1.0";
+		type = "mod";
+		inputs = "";
+		dependencies[] = 
+		{  
+			"World"
+		};
+		class defs
+		{
+			class worldScriptModule
+			{
+				files[] = 
+				{ 
+					"SynergyModPack/scripts/4_World",
+					"SynergyModPack/scripts/Common"
+				};
+			};
+		};
+	};
+};
+class CfgVehicles
+{
+	class FireplaceBase;
+	class Fireplace: FireplaceBase {};
+	class Zen_EternalFireplace: Fireplace {};
+};
---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "Synergy Mod Pack"; // name
+picture = ""; // picture in expanded description
+logoSmall = ""; // icon next to mod name when description is not expanded
+logo = ""; // logo below game menu
+logoOver = ""; // on mouse hover over logo
+tooltip = "Synergy Mod Pack"; // tool tip on mouse hover
+overview = ""; // overview
+action = ""; // link
+author = "colabus"; // author
+version = "1.0"; // version
